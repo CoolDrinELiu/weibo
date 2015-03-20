b8c698689cb8285b2ae3545ac874d185323044da
DrinE <DrinE.Liu@gmail.com>
2015-03-20 21:12:12 +0800

    Backup for gettin back 2 school, damn school shitz~!(undone: db migrate command)

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord:: Base
 
   def feed
 # This is preliminary. See "Following users" for the full implementation.
-    Micropost. where("user_id = ?", id)
+    Micropost.where("user_id = ?", id)
   end
 
   private
